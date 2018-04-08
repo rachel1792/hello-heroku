14c94270e2d683b2ffca49b000910cef352edd53
Rachel Kogan <rachelkogan@Rachels-MacBook-Air.local>
2018-04-08 14:24:30 -0400

    fab test migrate works

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,33 @@
 import os
 import sys
 
-from fabric.colors import green, red, blue, magenta
+from fabric.colors import green, red, blue
 
-from fabric.decorators import task, runs_once, parallel
-from fabric.operations import prompt, local
+from fabric.decorators import task, runs_once
+from fabric.operations import local
 from fabric.state import env as fab_env
+
+
+@runs_once
+def _set_current_environment(env):
+    """sets the current environment for this invocation of Fabric
+    Runs only once, and is intended to be called by the first task"""
+
+    print('setting Fabric environment to {}'.format(env))
+    fab_env['environment'] = env
+
+    print('setting Olympus config to {}'.format(fab_env['environment']))
+    os.environ['CONFIG_ENV'] = './config/{}.yaml'.format(fab_env['environment'])
+
+    print('clear local GCE cache')
+    local('rm -f ~/.gcetools/instances')
+
+
+@task
+def test():
+    """Load test configuration"""
+    _set_current_environment('test')
+
 
 
 @task()
@@ -45,6 +67,19 @@ def db():
             config.get('database.name'),
         ),
     )
+
+
+@task
+def migrate(command='upgrade head'):
+    print green('Running migrations'.format(fab_env['environment']))
+
+    # Migrate tables
+    res = local('python manage.py db {}'.format(command))
+    if not res.succeeded:
+        print red('Failed to migrate tables.')
+        return
+
+    print green('Successfully migrated the database.')
 
 
 @task
